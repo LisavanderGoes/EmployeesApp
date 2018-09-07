5479a91529695b954257f571f6cef686f2870774
Lisa <lisavandergoes@gmail.com>
2018-09-07 10:02:02 +0200

    Strings changed

--- EmployeesApp/EmployeesApp/Values/Strings.swift
+++ EmployeesApp/EmployeesApp/Values/Strings.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Strings {
     
-    public static let dataLoginCredentialsIsEmptyMessage: String = "There are no users! Are you the first?"
+    public static let dataLoginCredentialsIsEmptyMessage: String = "Nothing was found!"
     public static let loginCredentialsCheckFailed: String = "Something does not match!"
     public static let checkHasSucceedMessage: String = "Succes"
 }

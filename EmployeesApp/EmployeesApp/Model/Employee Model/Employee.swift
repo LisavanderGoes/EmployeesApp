fa3a976f1c84b23c34c90ac8492f357960ed5767
Lisa <lisavandergoes@gmail.com>
2018-09-12 10:00:27 +0200

    Added Employee struct
    + Occupation enum

---
+++ EmployeesApp/EmployeesApp/Model/Employee Model/Employee.swift
@@ -0,0 +1,12 @@
+//
+//  Created by WebIQ Stagiaire on 12-09-18.
+//  Copyright © 2018 Lisa van der Goes. All rights reserved.
+//
+
+import Foundation
+
+struct Employee {
+    var name: String
+    var occupation: Occupation
+    var emailAddress: String
+}
---
+++ EmployeesApp/EmployeesApp/Model/Employee Model/Enum/Occupation.swift
@@ -0,0 +1,12 @@
+//
+//  Created by WebIQ Stagiaire on 12-09-18.
+//  Copyright © 2018 Lisa van der Goes. All rights reserved.
+//
+
+import Foundation
+
+enum Occupation {
+    case Backend_Developer
+    case Frontend_Developer
+    case iOS_Developer
+}
